55f1aabbe299956af8386f0a18a4e5de28836301
Luke Stephen Schierer <luke@schierer.org>
2022-01-04 14:39:09 -0500

    create a bucket that should get destroyed

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,11 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 
-export class InfrastructureStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+		const bucket = new s3.Bucket(this, 'HPBucket', {
+			autoDeleteObjects: true,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			websiteIndexDocument: 'index.html',
+			websiteErrorDocument: '404.html',
+			enforceSSL: true,
+			publicReadAccess: true,
+		});
 
   }
 }
