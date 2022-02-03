41f6a0059bc5193ba6e5d193bb4cb16f60ad966b
Luke Stephen Schierer <luke@schierer.org>
2022-02-02 19:37:06 -0500

    I do not need yet another pipeline or repo at this level

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -8,30 +8,6 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-		const repo = new codecommit.Repository(this, 'HPRepo', {
-			repositoryName: 'HP_Stuff',
-			description: 'Luke Schierer\'s Harry Potter Stuff',
-		});
-
-		const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-			pipelineName: 'HP_Pipeline',
-			crossAccountKeys: false,
-			synth: new pipelines.CodeBuildStep('Synth', {
-				input: pipelines.CodePipelineSource.codeCommit(repo , 'master'),
-        primaryOutputDirectory: 'infrastructure/cdk.out',
-        installCommands: [
-          'cd infrastructure', 
-          'apt-get update',
-          'curl -L -o hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.91.2/hugo_0.91.2_Linux-64bit.deb',
-          'dpkg -i hugo.deb',
-        ],
-				commands: [
-          'npm ci', 
-          'npm run build', 
-          'npx cdk synth',
-        ],
-			}),
-		});
 
   }
 }
