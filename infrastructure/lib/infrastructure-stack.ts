26bc1bf6f9acb1ac71f79fd7bb0c6f22a5d5596c
Luke Stephen Schierer <luke@schierer.org>
2022-02-03 17:14:53 -0500

    create a bucket, maybe?

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,13 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import *as pipelines from 'aws-cdk-lib/pipelines';
-import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import { aws_cloudformation as cfn } from 'aws-cdk-lib';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as cloudwatch from "aws-cdk-lib/aws-cloudwatch";
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const HugoSute = 'hpfan.schierer.org';
+
+    const SOZone = route53.HostedZone.fromHostedZoneAttributes(this, 'SOZone', {
+      zoneName: 'schierer.org',
+      hostedZoneId: 'ZOB4NXMJR2BZF',
+    });
+
+    const HPBucket = new s3.Bucket(this, 'HPHugoBucket', {
+      encryption: s3.BucketEncryption.KMS,
+      bucketKeyEnabled: true,
+      enforceSSL: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      removalPolicy: RemovalPolicy.DESTROY, //safe since everything in here is generated
+      autoDeleteObjects: true, // safe since everything in here is generated
+    });
+
+    /*const HPRecord = new route53.ARecord(this, 'Alias', {
+      zone: SOZone,
+      recordName: HugoSute, 
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+    });
+    */
 
   }
 }
