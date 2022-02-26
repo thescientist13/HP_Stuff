519332ab46aa39812fa4ad0f60b3baae03ab0dcb
Luke Schierer <luke@schierer.org>
2022-02-26 14:38:51 -0500

    save this for now

---
+++ assets/js/gedcom.js
@@ -0,0 +1,12 @@
+import { readGedcom } from 'read-gedcom';
+import fs from 'fs';
+
+/* example from https://docs.arbre.app/read-gedcom/pages/Getting%20Started/quickstart.html
+fs.readFile('your_file_here.ged', (error, buffer) => {
+  if (error) throw error;
+  const gedcom = readGedcom(buffer);
+  console.log(gedcom.getHeader().toString());
+});
+*/
+
+
