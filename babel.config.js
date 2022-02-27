f418a203157d000a3efee52a970ee136f377b489
Luke Stephen Schierer <luke@schierer.org>
2022-02-27 02:51:31 -0500

    allow me to use typescript instead of js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   const presets = [
-    ["@babel/preset-react"]
+    ["@babel/preset-react", "@babel/preset-typescript"]
     ]
   const plugins = [];
   return {
