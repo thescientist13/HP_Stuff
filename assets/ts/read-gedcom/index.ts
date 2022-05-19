9b1173dee43655614586313f91a9699c875fe0a9
Luke Schierer <lschiere@amazon.com>
2022-05-19 17:57:27 -0400

    typescript requires types

--- assets/ts/read-gedcom/index.ts
+++ assets/ts/read-gedcom/index.ts
@@ -1,10 +1,7 @@
 import Alpine from 'alpinejs';
 
-import intersect from '@alpinejs/intersect'
-import persist from '@alpinejs/persist'
-
-
-window.Alpine = Alpine
+//window.Alpine = Alpine
+//cannot have hte above without type tomfoolery that I have not figured out yet
 
 Alpine.store('testmessage', {
 	message: 'this is a test of alpine with typescript',
