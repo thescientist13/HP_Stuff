54d450c23f979572f0d78de66bd5914cb9c17a9e
Luke Schierer <lschiere@amazon.com>
2022-05-20 18:55:00 -0400

    current state with a file open function untested

--- assets/ts/read-gedcom/index.ts
+++ assets/ts/read-gedcom/index.ts
@@ -1,4 +1,8 @@
 import Alpine from 'alpinejs';
+import * as rgc from 'read-gedcom';
+import * as buffer from 'buffer';
+
+interface CallbackType { (Arg: string): void }
 
 //window.Alpine = Alpine
 //cannot have hte above without type tomfoolery that I have not figured out yet
@@ -8,5 +12,20 @@ Alpine.store('testmessage', {
 });
 
 Alpine.start();
+
+Alpine.initGedCom(myfile: string, callback: CallbackType, ErrText: string) {
+  var XMLHttpRequest = require("xmlhttprequest-ssl").XMLHttpRequest;
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      callback(this.responseText);
+    }
+  }
+
+  xhr.open("GET", url);
+  xhr.send();
+}
 
 
