8abb56ed56a3accc08ce4f3deee8050c3e8eafa0
Luke Schierer <lschiere@amazon.com>
2022-06-10 17:13:46 -0400

    still trying to figure out alpinejs

--- assets/ts/read-gedcom/index.ts
+++ assets/ts/read-gedcom/index.ts
@@ -1,8 +1,10 @@
-import Alpine from 'alpinejs';
+import  Alpine from 'alpinejs';
 import * as rgc from 'read-gedcom';
 import * as buffer from 'buffer';
 
-interface CallbackType { (Arg: string): void }
+interface CallbackType { 
+  (Arg: string): void; 
+}
 
 //window.Alpine = Alpine
 //cannot have hte above without type tomfoolery that I have not figured out yet
@@ -13,19 +15,45 @@ Alpine.store('testmessage', {
 
 Alpine.start();
 
-Alpine.initGedCom(myfile: string, callback: CallbackType, ErrText: string) {
-  var XMLHttpRequest = require("xmlhttprequest-ssl").XMLHttpRequest;
+Alpine.directive('HPGedCom', el => {
+  var GC = new HPGC;
+});
 
-  var xhr = new XMLHttpRequest();
+export class HPGC {
 
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4) {
-      callback(this.responseText);
-    }
+  private loadGedCom(url, timeout, callback) {
+    var args = Array.prototype.slice.call(arguments, 3);
+    var xhr = new XMLHttpRequest();
+    xhr.ontimeout = function () {
+      console.error("The request for " + url + " timed out.");
+    };
+
+    xhr.onload = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          callback.apply(xhr, args);
+        } else {
+          console.error(xhr.statusText);
+        }
+      }
+    };
+    xhr.open("GET", url);
+    xhr.timeout = timeout;
+    xhr.send();
   }
 
-  xhr.open("GET", url);
-  xhr.send();
+  private _initGedCom( gedFile) {
+    console.log("callback function called");
+  }
+
+  public initGedCom(myURL: string) {
+
+    var gedFile = myURL + "Harrypedia/potter_universe.ged";
+    loadGedCom(gedFile, 2000, _initGedCom);
+
+  }
+    
 }
 
+// vim: shiftwidth=2:tabstop=2:expandtab 
 
