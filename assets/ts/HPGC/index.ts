88869d9c789f9bad95813ab7701b9c4a420c0b05
Luke Schierer <lschiere@amazon.com>
2022-06-15 21:46:07 -0400

    giving up for the night, still getting errors when the browser tries to load the npm module

--- assets/ts/HPGC/index.ts
+++ assets/ts/HPGC/index.ts
@@ -1,3 +1,4 @@
+import Alpine from 'alpinejs';
 import fetch from 'isomorphic-fetch';
 import * as rgc from 'read-gedcom';
 
@@ -95,6 +96,19 @@ export class HPGC {
   };
   
 };
+
+interface CallbackType {
+  (Arg: string): void;
+}
+
+//window.Alpine = Alpine
+//cannot have the above without type tomfoolery that I have not figured out yet
+
+Alpine.store('testmessage', {
+  message: 'this is a test of alpine with typescript',
+});
+
+Alpine.start();
 
 // vim: shiftwidth=2:tabstop=2:expandtab
 
