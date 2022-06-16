7dc3c9de174709aee2b22d58cd9ef9e2bfba635d
Luke Schierer <lschiere@amazon.com>
2022-06-16 15:54:36 -0400

    based on https://github.com/bep/docuapi/blob/master/layouts/partials/js.html I realized some of what I was doing wrong, but not enough

--- assets/ts/HPGC/index.ts
+++ assets/ts/HPGC/index.ts
@@ -97,18 +97,5 @@ export class HPGC {
   
 };
 
-interface CallbackType {
-  (Arg: string): void;
-}
-
-//window.Alpine = Alpine
-//cannot have the above without type tomfoolery that I have not figured out yet
-
-Alpine.store('testmessage', {
-  message: 'this is a test of alpine with typescript',
-});
-
-Alpine.start();
-
 // vim: shiftwidth=2:tabstop=2:expandtab
 
