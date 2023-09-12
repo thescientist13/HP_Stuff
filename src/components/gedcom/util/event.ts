2b112bb5a1acfd347ffe056f93230fabd69dca31
Luke Schierer <lschiere@amazon.com>
2023-09-12 14:01:26 -0400

    one file appears to be converted to lit

--- src/components/gedcom/util/event.ts
+++ src/components/gedcom/util/event.ts
@@ -1,7 +1,9 @@
-import type { ValueDate, SelectionDate, } from 'read-gedcom';
+import type { ValueDate, SelectionDate, SelectionDateExact, SelectionEvent } from 'read-gedcom';
 import { toJsDate } from 'read-gedcom';
+import { DateTime as LuxDateTime } from 'luxon';
+import { html } from 'lit';
 
-function isValidDate(dateObject){
+function isValidDate(dateObject: Date){
     return dateObject.toString() !== 'Invalid Date';
 }
 
@@ -11,86 +13,58 @@ export function displayDate(dateGedcom: SelectionDate, isShort = false) {
     if (obj !== null) {
         if (obj.isDatePunctual && obj.date.calendar.isGregorian && !obj.date.year.isDual) {
             const jsDate = toJsDate(obj.date);
-            if(isValidDate(jsDate)) {
-                return (
-                    <>
-                        {obj.isDateApproximate && (
-                            <>
-                                {'about '}
-                            </>
-                        )}
-                        <FormattedDate
-                            value={jsDate}
-                            year="numeric"
-                            month={!date.month || isShort ? undefined : 'long'}
-                            day={!date.day || isShort ? undefined : 'numeric'}
-                            timeZone="UTC"
-                        />
-                    </>
-                );
+            if(jsDate && isValidDate(jsDate)) {
+              const luxDate = LuxDateTime.fromJSDate(jsDate, {});
+              let template = html``;
+              if (obj.isDateApproximated) {
+                template = html`about `;
+              }
+              template = html`${template}<time datetime=${luxDate.toISO()} >${luxDate.toRelative()}</time>`;
+              return template;
             } else {
-                return first[0].value;
+                return html`first[0].value`;
             }
 
         } else {
-            return first[0].value;
+            return html`first[0].value`;
         }
     } else {
-        return first[0].value;
+        return html`first[0].value`;
     }
 }
 
-export function isEventEmpty(eventGedcom, withDate = true, withPlace = true) {
+export function isEventEmpty(eventGedcom: SelectionEvent, withDate = true, withPlace = true) {
     return !(
         (eventGedcom.getDate().value()[0] && withDate)
         || (eventGedcom.getPlace().value()[0] && withPlace)
     );
 }
 
-export function displayDateExact(dateGedcom, withTime) {
+export function displayDateExact(dateGedcom: SelectionDateExact, withTime: boolean) {
     const date = dateGedcom.valueAsExactDate()[0];
     const time = dateGedcom.getExactTime().valueAsExactTime()[0];
     if(date) {
         const jsDate = new Date(
-            parseInt(date.year),
-            parseInt(date.month) - 1,
-            parseInt(date.day)
+          date.year,
+          date.month - 1,
+          date.day
         );
         if(isValidDate(jsDate)) {
             if(withTime && time) {
                 jsDate.setHours(
-                    parseInt(time.hours),
-                    parseInt(time.minutes),
-                    time.seconds !== undefined ? parseInt(time.seconds) : 0,
-                    time.centiseconds !== undefined ? parseInt(time.centiseconds) * 10 : 0,
+                    time.hours,
+                    time.minutes,
+                    time.seconds !== undefined ? time.seconds : 0,
+                    time.centiseconds !== undefined ? time.centiseconds * 10 : 0,
                 );
-                return (
-                    <FormattedDate
-                        value={jsDate}
-                        year="numeric"
-                        month="long"
-                        day="numeric"
-                        hour="numeric"
-                        minute="numeric"
-                        second="numeric"
-                        timeZone="UTC"
-                    />
-                );
-            } else {
-                return (
-                    <FormattedDate
-                        value={jsDate}
-                        year="numeric"
-                        month="long"
-                        day="numeric"
-                        timeZone="UTC"
-                    />
-                );
+                
             }
+            const luxDate = LuxDateTime.fromJSDate(jsDate, {});
+            return html`<time datetime=${luxDate.toISO()} >${luxDate.toRelative()}</time>`;
         } else {
-            return dateGedcom.value()[0];
+            return html`dateGedcom.value()[0]`;
         }
     } else {
-        return dateGedcom.value()[0];
+        return html`dateGedcom.value()[0]`;
     }
 }
