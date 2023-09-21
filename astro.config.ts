83108962607d4557d3a8daf1d83c25f1993d8d4f
Luke Schierer <luke@schierer.org>
2023-09-20 23:46:32 -0400

    set up to use tailwind and bootstrap

--- astro.config.ts
+++ astro.config.ts
@@ -1,81 +1,89 @@
 import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import lit from "@astrojs/lit";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hpfan.schierer.org/",
-  integrations: [
-    starlight({
-      title: "Luke's HP Site",
-      logo: {
-        src: './src/assets/LukeHPSite.svg',
-        replacesTitle: true,
-      },
-      lastUpdated: true,
-      sidebar: [
-        {
-          label: "Encyclopedic Reference",
-          autogenerate: {
-            directory: 'Harrypedia',
-            collapsed: true,
-          },
-          collapsed: true,
-        },
-        {
-          label: "Bookmarks",
-          autogenerate: {
-            directory: 'Bookmarks',
-            collapsed: true,
-          },
-          collapsed: true,
-        },
-        {
-          label: 'Fan Fiction',
-          collapsed: true,
-          items: [
-            {
-              label: 'Harry Potter - Nephilim',
-              items:[
+    site: "https://hpfan.schierer.org/",
+    integrations: [
+        starlight({
+            title: "Luke's HP Site",
+            customCss: [
+                './src/styles/tailwind.css',
+            ],
+            logo: {
+                src: './src/assets/LukeHPSite.svg',
+                replacesTitle: true
+            },
+            lastUpdated: true,
+            sidebar: [
                 {
-                  label: 'Introduction',
-                  link: '/fanfiction/harry_potter_-_nephilim/introduction/'
+                    label: "Encyclopedic Reference",
+                    autogenerate: {
+                        directory: 'Harrypedia',
+                        collapsed: true
+                    },
+                    collapsed: true
                 },
                 {
-                  label: 'Prologue',
-                  link: '/fanfiction/harry_potter_-_nephilim/prologue/'
+                    label: "Bookmarks",
+                    autogenerate: {
+                        directory: 'Bookmarks',
+                        collapsed: true
+                    },
+                    collapsed: true
                 },
                 {
-                  label: 'Chapters',
-                  autogenerate: {
-                    directory: 'FanFiction/Harry_Potter_-_Nephilim/Chapters',
-                  },
-                },
-                {
-                  label: 'Back Story',
-                  autogenerate: {
-                    directory: 'FanFiction/Harry_Potter_-_Nephilim/backstory',
-                  },
-                  collapsed: true,
-                },
-                {
-                  label: 'Appendices',
-                  autogenerate: {
-                    directory: 'FanFiction/Harry_Potter_-_Nephilim/Appendices',
-                  },
-                },
-              ]
-            }
-          ]
-        }]
-    }),
-    lit()
-  ],
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp'
+                    label: 'Fan Fiction',
+                    collapsed: true,
+                    items: [
+                        {
+                            label: 'Harry Potter - Nephilim',
+                            items: [
+                                {
+                                    label: 'Introduction',
+                                    link: '/fanfiction/harry_potter_-_nephilim/introduction/'
+                                },
+                                {
+                                    label: 'Prologue',
+                                    link: '/fanfiction/harry_potter_-_nephilim/prologue/'
+                                },
+                                {
+                                    label: 'Chapters',
+                                    autogenerate: {
+                                        directory: 'FanFiction/Harry_Potter_-_Nephilim/Chapters'
+                                    }
+                                },
+                                {
+                                    label: 'Back Story',
+                                    autogenerate: {
+                                        directory: 'FanFiction/Harry_Potter_-_Nephilim/backstory'
+                                    },
+                                    collapsed: true
+                                },
+                                {
+                                    label: 'Appendices',
+                                    autogenerate: {
+                                        directory: 'FanFiction/Harry_Potter_-_Nephilim/Appendices'
+                                    }
+                                },
+                            ]
+                        },
+                    ]
+                }
+            ]
+        }),
+        lit(),
+        tailwind({
+            applyBaseStyles: false,
+        })
+    ],
+    // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp'
+        }
     }
-  }
 });
---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    syntax: 'postcss-lit',
+    plugins: {
+        tailwindcss: {
+            config: './tailwind.config.cjs'
+        }
+    }
+};
