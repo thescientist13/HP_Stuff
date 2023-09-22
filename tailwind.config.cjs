fbf67f28b8e026cb50b2c2228f8d5561d2674a08
Luke Schierer <luke@schierer.org>
2023-09-21 22:34:54 -0400

    base as suggested by starlight

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ const {tailwindTransform} = require('postcss-lit');
 const starlightPlugin = require('@astrojs/starlight-tailwind');
 const fontawesome = require('tailwind-fontawesome');
 
+// Generated color palettes
+const accent = { 200: '#a2ccff', 600: '#006bc6', 900: '#003262', 950: '#002449' };
+const gray = { 100: '#f1f7fc', 200: '#e2f0f9', 300: '#b4c5cf', 400: '#7090a5', 500: '#3e5c6f', 700: '#1e3c4d', 800: '#0b2a3b', 900: '#0d1a22' };
+
 module.exports = {
   content: {
     files: ['./*.js'],
@@ -9,10 +13,15 @@ module.exports = {
       ts: tailwindTransform
     }
   },
-	plugins: [
-		starlightPlugin(),
-		fontawesome({
-			version: 6
-		})
-	],
+  theme: {
+    extend: {
+      colors: { accent, gray },
+    },
+  },
+  plugins: [
+    starlightPlugin(),
+    fontawesome({
+      version: 6
+    })
+  ],
 };
