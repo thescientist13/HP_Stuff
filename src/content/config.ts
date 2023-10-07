b906f7c64ddb815ba70477d0d310829d8184c26f
Luke Schierer <luke@schierer.org>
2023-10-07 10:34:26 -0400

    this works better

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
-import {  defineCollection } from 'astro:content';
-import { z } from "zod";
-
+import {type SchemaContext, z, defineCollection } from 'astro:content';
 import { DatabaseSchema } from '@lib/GrampsZodTypes';
+
+const originalHero = (ctx: SchemaContext) => docsSchema()(ctx).pick({ hero: true});
 
 export const collections = {
   docs: defineCollection({
     schema: (ctx) =>
         docsSchema()(ctx).extend({
           // Add a new optional field to the schema.
-          grampsId: z.string().optional(),
+          hero: z.union([z.string(),originalHero(ctx)]).optional(),
         }),
   }),
   i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
