3f7175c1ba0985cc338ee0cfecd6a32d7b0340cd
Luke Schierer <luke@schierer.org>
2023-10-12 01:33:27 -0400

    continuing work towards a history content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,25 @@ import { DatabaseSchema } from '@lib/GrampsZodTypes';
 
 const originalHero = (ctx: SchemaContext) => docsSchema()(ctx).pick({ hero: true});
 
+export const historySchema = z.object({
+    date: z.date().optional(),
+    type: z.string(),
+    source: z.string(),
+    blurb: z.string(),
+    description: z.string().optional(),
+});
+
+export type history = z.infer<typeof historySchema>;
+
 export const collections = {
-  docs: defineCollection({
-    schema: (ctx) =>
-        docsSchema()(ctx).extend({
-          // Add a new optional field to the schema.
-          hero: z.union([z.string(),originalHero(ctx)]).optional(),
-        }),
-  }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-  gramps: defineCollection({type: 'data', schema: DatabaseSchema})
+    docs: defineCollection({
+            schema: (ctx) =>
+                docsSchema()(ctx).extend({
+                    // Add a new optional field to the schema.
+                    hero: z.union([z.string(),originalHero(ctx)]).optional(),
+                }),
+    }),
+    i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+    gramps: defineCollection({type: 'data', schema: DatabaseSchema}),
+    history: defineCollection({type: 'data', schema: historySchema}),
 };
