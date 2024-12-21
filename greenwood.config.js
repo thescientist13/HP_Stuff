a35880db6c7099a41edd52098ceaf2a1394e1f37
Owen Buckley <owenbuckley13@gmail.com>
2024-12-21 12:07:43 -0500

    patching support for no-prerender option of data client

--- greenwood.config.js
+++ greenwood.config.js
@@ -2,7 +2,6 @@ import { greenwoodPluginTypeScript } from "@greenwood/plugin-typescript";
 import { greenwoodPluginPostCss } from "@greenwood/plugin-postcss";
 import { greenwoodPluginGoogleAnalytics } from "@greenwood/plugin-google-analytics";
 import { greenwoodPluginImportRaw } from "@greenwood/plugin-import-raw";
-import { greenwoodPluginRendererLit } from "@greenwood/plugin-renderer-lit";
 /*
  * this plugin appears to do more than I first understood from the documentation.
  * Once enabled, I it fully replaces WCC and I *think* you can no longer use html components at all.
@@ -21,8 +20,6 @@ import { unified } from "unified";
 export default {
   activeContent: true,
   isolation: true,
-  // this is required for activeContent's data client feature
-  // prerender: true,
   staticRouter: false,
   markdown: {
     plugins: ["unified", "remark-gfm"],
@@ -38,7 +35,6 @@ export default {
     greenwoodPluginGoogleAnalytics({
       analyticsId: "G-9KF1R3YFTZ",
     }),
-    greenwoodPluginImportRaw(),
-    greenwoodPluginRendererLit()
+    greenwoodPluginImportRaw()
   ],
 };
