c0696681f4c01f64ae9e177bb457c6bda16fd424
Luke Schierer <luke@schierer.org>
2025-01-01 18:50:45 -0500

    current progress

--- bin/nodeSetup.js
+++ bin/nodeSetup.js
@@ -2,3 +2,12 @@ import { register } from "node:module";
 import { pathToFileURL } from "node:url";
 
 register("./node_modules/@greenwood/cli/src/loader.js", pathToFileURL("./"));
+
+const application = await import("@greenwood/cli/src/index.js");
+
+/*
+ * process.argv is the array
+ * [0] points to node
+ * [1] points to the script
+ * [2] is the first true parameter/argument
+ */
